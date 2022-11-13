28a3e772a431f9770a4c2da5b41a662c8c7ed6c7
root <peechupreethi@gmail.com>
2022-11-13 15:55:38 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+
+import {Route,Switch} from 'react-router-dom'
+
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import TeamMatches from './components/TeamMatches'
+
+import './App.css'
+
+const App = () => (
+    <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/team-matches/:id/" component={TeamMatches} />
+    <Route component={NotFound} />
+    </Switch>
+)
+
+export default App
